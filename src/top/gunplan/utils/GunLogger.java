f8d5a8d00992e69a5c0493ddaaf4b27558da1533
frank <lnpj.office@icloud.com>
2019-07-21 09:09:03 +0800

    version 1.0.2.g +update some things

---
+++ src/top/gunplan/utils/GunLogger.java
@@ -0,0 +1,58 @@
+package top.gunplan.utils;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URISyntaxException;
+
+
+/**
+ * GunLogger
+ * <p>
+ * this is a log interface
+ *
+ * @author frank albert
+ * @version 0.0.0.1
+ * @date 2019-07-21 08:43
+ */
+
+public interface GunLogger {
+
+    /**
+     * <p>
+     * set format
+     * </p>
+     *
+     * @param format format
+     */
+    void setFormat(String format);
+
+    GunLogger setTAG(Class<?> tag);
+
+
+    void setErrOutput(OutputStream erroutput);
+
+
+    void init();
+
+
+    void setStdoutput(OutputStream os);
+
+
+    void info(String s, String... val);
+
+    void debug(String s, String... val);
+
+
+    void error(String s, String... val);
+
+    void error(Exception s);
+
+
+    void urgency(String s, String... val);
+
+    void outputFile(String file) throws IOException, URISyntaxException;
+
+    void setLevel(int level);
+
+
+}
---
+++ src/top/gunplan/utils/GunNxLogger.java
@@ -0,0 +1,133 @@
+package top.gunplan.utils;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
+
+
+/**
+ * GunNxLogger a logger
+ *
+ * @author dosdrtt
+ * @version 2.0.0.2
+ */
+
+public final class GunNxLogger implements GunLogger {
+    /**
+     * this is s static block
+     */
+    private static final int BASE_LEVEL = 1;
+    private volatile int level = 0;
+    private AtomicReference<Class<?>> clazz;
+
+    private volatile SimpleDateFormat ffm = null;
+    private volatile OutputStream stdoutput;
+    private volatile OutputStream erroutput;
+
+    {
+        stdoutput = System.out;
+        erroutput = System.err;
+    }
+
+    @Override
+    public void setFormat(String format) {
+        ffm = new SimpleDateFormat(format);
+    }
+
+    @Override
+    public GunLogger setTAG(Class<?> tag) {
+        clazz.setRelease(tag);
+        return this;
+    }
+
+    @Override
+    public synchronized void setErrOutput(OutputStream erroutput) {
+        this.erroutput = erroutput;
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    private void realPrint(final OutputStream os, final String tag, String content, String... val) {
+        String op = tag;
+        String className = "[" + (clazz == null ? "null" : clazz.get().getSimpleName()) + "]";
+        if (val != null && val.length != 0) {
+            StringBuilder contentBuilder = new StringBuilder();
+            for (String aVal : val) {
+                contentBuilder.append(aVal);
+            }
+            op = op + contentBuilder.toString() + className + " " + content;
+        } else {
+            op = op + className + content;
+        }
+        try {
+            os.write(("[" + (ffm == null ? System.currentTimeMillis() : ffm.format(new Date())) + "] " + op + "\n").getBytes());
+            stdoutput.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public synchronized void setStdoutput(OutputStream os) {
+        this.stdoutput = os;
+    }
+
+
+    @Override
+    public void info(String s, String... val) {
+        if (this.level <= (BASE_LEVEL)) {
+            realPrint(stdoutput, "[INFO] ", s, val);
+        }
+    }
+
+    @Override
+    public void debug(String s, String... val) {
+
+        if (this.level <= (BASE_LEVEL << 1)) {
+            realPrint(stdoutput, "[DEBUG] ", s, val);
+        }
+    }
+
+    @Override
+    public void error(String s, String... val) {
+        if (this.level <= (BASE_LEVEL << 2)) {
+            realPrint(erroutput, "[ERROR] ", s, val);
+        }
+    }
+
+    @Override
+    public void error(Exception s) {
+        if (this.level <= (BASE_LEVEL << 2)) {
+            realPrint(erroutput, "[ERROR] ", s.getMessage());
+        }
+    }
+
+    @Override
+    public void urgency(String s, String... val) {
+        if (this.level <= (BASE_LEVEL << 3)) {
+            realPrint(erroutput, "[URGENCY] ", s, val);
+        }
+    }
+
+    @Override
+    public void outputFile(String file) throws IOException, URISyntaxException {
+        byte[] property = Files.readAllBytes(Paths.get(Objects.requireNonNull(this.getClass().getResource(file)).toURI()));
+        realPrint(stdoutput, "", new String(property));
+    }
+
+
+    @Override
+    public void setLevel(int level) {
+        this.level = level;
+    }
+}
+
